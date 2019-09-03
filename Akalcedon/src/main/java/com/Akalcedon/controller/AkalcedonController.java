0bd5cbac9ea867ec9b8b0cadeb6f46905a1c0d1b
erhanianwar <erhanianwar@mbp-de-erhani>
2019-09-03 13:23:30 +0100

    Nouveau theme

--- Akalcedon/src/main/java/com/Akalcedon/controller/AkalcedonController.java
+++ Akalcedon/src/main/java/com/Akalcedon/controller/AkalcedonController.java
@@ -16,6 +16,40 @@ public class AkalcedonController {
 	public String accueil() {
 		return "accueil";
 	}
+	
+	@GetMapping("/rechercheEtudiant") 
+	public String rechercheEtudiant() {
+		return "rechercheEtudiant";
+	}
+	
+	
+	@GetMapping("/AjoutEtudiant") 
+	public String AjoutEtudiant() {
+		return "AjoutEtudiant";
+	}
 
+	
+	@GetMapping("/statistique") 
+	public String statistique() {
+		return "statistique";
+	}
+	
+	
+	@GetMapping("/evenement") 
+	public String evenement() {
+		return "evenement";
+	}
+	
+	@GetMapping("/profilEtudiant") 
+	public String profilEtudiant() {
+		return "profilEtudiant";
+	}
+	
+	
+	@GetMapping("/postNotes") 
+	public String postNotes() {
+		return "postNotes";
+	}
+	
 }
  
